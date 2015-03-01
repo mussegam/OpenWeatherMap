6feec1c978d7a9e4ceb58af8e83aead315529de0
mussegam <austorm@gmail.com>
2015-03-01 14:41:37 +0100

    Initial Commit

---
+++ OpenWeatherMap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  OpenWeatherMap
+//
+//  Created by Javi Dolcet Figueras on 01/03/15.
+//  Copyright (c) 2015 Teanamics. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
